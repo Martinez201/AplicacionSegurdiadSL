de2470c195d591665fc4e4d542cbe80d0786d957
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-05-13 12:33:05 +0200

    Creada clase del listado de clientes

---
+++ app/src/main/java/com/example/proyectoampliacion/Classes_Auxiliares/Cliente.kt
@@ -0,0 +1,4 @@
+package com.example.proyectoampliacion.Classes_Auxiliares
+
+class Cliente(var nombre:String,var apellidos:String, var direccion:String, var ciudad:String,var provincia:String,var telefono:String, var estado:Boolean) {
+}
