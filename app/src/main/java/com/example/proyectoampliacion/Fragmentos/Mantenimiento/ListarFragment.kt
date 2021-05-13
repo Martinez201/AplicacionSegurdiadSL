d32506c760886a1263c6d4cb69bb4a12b737d347
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-05-13 14:12:18 +0200

    Añadida vista de los  listados

--- app/src/main/java/com/example/proyectoampliacion/Fragmentos/Mantenimiento/ListarFragment.kt
+++ app/src/main/java/com/example/proyectoampliacion/Fragmentos/Mantenimiento/ListarFragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.proyectoampliacion.R
+import kotlinx.android.synthetic.main.fragment_altas.*
+import kotlinx.android.synthetic.main.fragment_bajas.*
+import kotlinx.android.synthetic.main.fragment_listar.*
 
 class ListarFragment : Fragment() {
 
@@ -20,6 +29,85 @@ class ListarFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_listar, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        arguments?.let {
+
+
+
+            when(it.getInt("tipo")){
+
+                0->{
+                    tvTituloLis.setText("Listados Albaranes");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Albaranes";
+                }
+                1->{
+                    tvTituloLis.setText("Listados Almacén");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Almacén";
+                }
+                2->{
+                    tvTituloLis.setText("Listados Clientes");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Clientes";
+                }
+                3->{
+                    tvTituloLis.setText("Listados Delegación");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Delegación";
+                }
+                4->{
+                    tvTituloLis.setText("Listados Empleados");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Empleados";
+                }
+                5->{
+                    tvTituloLis.setText("Listados Facturas");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Facturas";
+                }
+                6->{
+                    tvTituloLis.setText("Listados Partes");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Partes";
+                }
+                7->{
+                    tvTituloLis.setText("Listados Presupuestos");
+                    (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Listados Presupuestos";
+                }
+            }
+
+        }
+
+        obtenerDatosVolley()
+    }
+
+
+    fun obtenerDatosVolley(){
+
+        val queue = Volley.newRequestQueue(this.context)
+        val url = "http://192.168.1.141/symfony/web/app.php/movil/clientes"
+
+        val jsObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url, null,
+                Response.Listener { response -> tvMostrar.text = response.toString()
+                },
+                Response.ErrorListener { error ->
+                    tvMostrar.text = "Comete una mierda"
+                }
+        )
+
+        queue.add(jsObjectRequest);
+
+
+        try {
+
+        } catch (e: Exception) {
+
+            Toast.makeText(this.context,e.message,Toast.LENGTH_SHORT).show();
+
+        } finally {
+
+        }
+
+
+    }
+
 
 
 }
