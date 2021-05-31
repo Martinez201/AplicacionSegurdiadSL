9b90fc3f64f4ea0b5b1e280a4fba54a05b8b11af
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-05-31 17:02:44 +0200

    Añadida estructura para la vista de Productos

---
+++ app/src/main/java/com/example/proyectoampliacion/Adaptadores/MiAdaptadorAlmacen.kt
@@ -0,0 +1,33 @@
+package com.example.proyectoampliacion.Adaptadores
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.proyectoampliacion.Classes_Auxiliares.Almacen
+import com.example.proyectoampliacion.Classes_Auxiliares.Cliente
+import com.example.proyectoampliacion.R
+import kotlinx.android.synthetic.main.vista_clientes.view.*
+import kotlinx.android.synthetic.main.vista_productos.view.*
+
+open class MiAdaptadorAlmacen(private val mContext:Context,private val listaProductos: List<Almacen>):ArrayAdapter<Almacen>(mContext,0,listaProductos) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        //return super.getView(position, convertView, parent)
+
+        val layout = LayoutInflater.from(mContext).inflate(R.layout.vista_clientes,parent,false);
+
+        val elementoActual = listaProductos[position];
+
+        layout.tvNombreP.text = elementoActual.nombre;
+        layout.tvPrecio.text = elementoActual.precio.toString();
+        layout.tvCantidad.text = elementoActual.cantidad.toString();
+        layout.tvTipo.text = elementoActual.tipo;
+
+
+        return layout;
+    }
+}
---
+++ app/src/main/java/com/example/proyectoampliacion/Classes_Auxiliares/Almacen.kt
@@ -0,0 +1,4 @@
+package com.example.proyectoampliacion.Classes_Auxiliares
+
+class Almacen(var id:Int, var nombre:String, var tipo:String, var precio:Double, var cantidad:Int,var imagen:String){
+}
