fcb5a6a2b981e2d1472cbe2e81dc96cccb8fbc46
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-05-31 20:49:56 +0200

    Añadida estructura para la vista de Presupuestos

---
+++ app/src/main/java/com/example/proyectoampliacion/Adaptadores/MiAdaptadorPresupuesto.kt
@@ -0,0 +1,46 @@
+package com.example.proyectoampliacion.Adaptadores
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.proyectoampliacion.Classes_Auxiliares.Presupuesto
+import com.example.proyectoampliacion.R
+import kotlinx.android.synthetic.main.vista_partes.view.*
+import kotlinx.android.synthetic.main.vista_presupuesto.view.*
+
+class MiAdaptadorPresupuesto(private val mContext: Context, private val listaPresupuestos: List<Presupuesto>): ArrayAdapter<Presupuesto>(mContext,0,listaPresupuestos) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        //return super.getView(position, convertView, parent)
+
+        val layout = LayoutInflater.from(mContext).inflate(R.layout.vista_clientes,parent,false);
+
+        val elementoActual = listaPresupuestos[position];
+
+        layout.tvEmpleadoPre.text = elementoActual.empleado.nombre + " " + elementoActual.empleado.apellidos;
+        layout.tvFechaPre.text = elementoActual.fecha;
+        layout.tvIntalacion.text = elementoActual.instalacion;
+        layout.tvContrato.text = elementoActual.contrato;
+
+
+        if (elementoActual.estado.equals("true")){
+
+            layout.btnEstadoP.text = "ABIERTO";
+            layout.btnEstadoP.setTextColor(Color.WHITE);
+            layout.btnEstadoP.setBackgroundColor(Color.GREEN);
+
+        }else{
+
+            layout.btnEstadoP.text = "CERRADO";
+            layout.btnEstadoP.setTextColor(Color.WHITE);
+            layout.btnEstadoP.setBackgroundColor(Color.YELLOW);
+        }
+
+
+        return layout;
+    }
+}
---
+++ app/src/main/java/com/example/proyectoampliacion/Classes_Auxiliares/Presupuesto.kt
@@ -0,0 +1,4 @@
+package com.example.proyectoampliacion.Classes_Auxiliares
+
+class Presupuesto (var id:Int, var empleado:Empleado, var fecha:String, var instalacion:String, var estado:String, var contrato:String){
+}
