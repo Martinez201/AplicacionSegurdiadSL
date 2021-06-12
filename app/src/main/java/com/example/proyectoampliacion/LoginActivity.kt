c5551dbc7df4836a9227a216ebb898cc1b6a9c90
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-06-12 13:34:27 +0200

    Añadida funcion para realizar peticiones HTTP POST de manera sincrona

--- app/src/main/java/com/example/proyectoampliacion/LoginActivity.kt
+++ app/src/main/java/com/example/proyectoampliacion/LoginActivity.kt
@@ -1,17 +1,24 @@
 package com.example.proyectoampliacion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.widget.Toast
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.*
+import org.json.JSONArray
 import org.json.JSONObject
+import java.io.IOException
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
         btnIniciar.setOnClickListener(){
 
@@ -22,29 +29,45 @@ class LoginActivity : AppCompatActivity() {
 
     fun comprobar(){
 
-        val queue = Volley.newRequestQueue(applicationContext)
-        val url = "http://192.168.1.141/symfony/web/app.php/movil/login"
+
+
+        var JSON:MediaType =  MediaType.get("application/json; charset=utf-8")
+
         val jsonObject= JSONObject();
 
         jsonObject.put("usuario",txtUsuario.text.toString());
         jsonObject.put("password",txtPassword.text.toString());
 
-        val jsonObjectRequest = JsonObjectRequest(url,jsonObject,
+        val client = OkHttpClient()
 
-            { response ->
+        val body: RequestBody = RequestBody.create(JSON,jsonObject.toString())
 
-                Toast.makeText(applicationContext,response.toString(), Toast.LENGTH_SHORT).show();
+        val request: Request = Request.Builder() //Create a request
+            .url("http://192.168.1.141/symfony/web/app.php/movil/login")
+            .post(body) //Indicated as get request
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .build()
 
-            },
+        var llamada: Call = client.newCall(request)
 
-            {   it->
+        try {
 
-                Toast.makeText(applicationContext,it.message.toString(), Toast.LENGTH_SHORT).show();
-            }
+            var response = llamada.execute()
 
-        )
 
-        queue.add(jsonObjectRequest);
+
+            val jsonArray = JSONObject(response.body()?.string())
+
+
+            Toast.makeText(applicationContext,jsonArray.toString(),Toast.LENGTH_SHORT).show()
+
+        }catch (e: IOException){
+
+            //Toast.makeText(applicationContext,e.message.toString(),Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 
 
