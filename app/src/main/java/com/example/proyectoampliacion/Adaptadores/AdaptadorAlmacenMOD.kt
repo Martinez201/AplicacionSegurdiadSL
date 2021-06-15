d972abd458820cbac14b3557d6faa37e12dd3507
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-06-15 12:04:01 +0200

    Anñadidas vista de modificar  almacen

---
+++ app/src/main/java/com/example/proyectoampliacion/Adaptadores/AdaptadorAlmacenMOD.kt
@@ -0,0 +1,36 @@
+package com.example.proyectoampliacion.Adaptadores
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.proyectoampliacion.Classes_Auxiliares.Almacen
+import com.example.proyectoampliacion.R
+import kotlinx.android.synthetic.main.vista_mod_productos.view.*
+
+class AdaptadorAlmacenMOD (private val mContext: Context, private val listaProductos: List<Almacen>):
+    ArrayAdapter<Almacen>(mContext,0,listaProductos) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        //return super.getView(position, convertView, parent)
+
+        val layout = LayoutInflater.from(mContext).inflate(R.layout.vista_mod_productos,parent,false);
+
+        val elementoActual = listaProductos[position];
+
+        layout.tvNombrePROMOD.text = elementoActual.nombre;
+        layout.tvPrecioPROMOD.text = elementoActual.precio.toString();
+        layout.tvCantidadPROMOD.text = elementoActual.cantidad.toString();
+        layout.tvTipoPROMOD.text = elementoActual.tipo;
+
+
+        layout.btnEditarPROMOD.setOnClickListener(){
+
+
+        }
+
+        return layout;
+    }
+}
