01ed9578d1915f44ec19430fb3b81616ae29dc48
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-06-15 12:23:28 +0200

    Añadida vista modificar empleados

---
+++ app/src/main/java/com/example/proyectoampliacion/Adaptadores/AdaptadorEmpleadosMOD.kt
@@ -0,0 +1,47 @@
+package com.example.proyectoampliacion.Adaptadores
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.proyectoampliacion.Classes_Auxiliares.Empleado
+import com.example.proyectoampliacion.R
+import kotlinx.android.synthetic.main.vista_empleados.view.*
+import kotlinx.android.synthetic.main.vista_mod_empleado.view.*
+
+class AdaptadorEmpleadosMOD   (private val mContext: Context, private val listaEmpleados: List<Empleado>): ArrayAdapter<Empleado>(mContext,0,listaEmpleados) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        //return super.getView(position, convertView, parent)
+
+        val layout = LayoutInflater.from(mContext).inflate(R.layout.vista_mod_empleado, parent, false);
+
+        val elementoActual = listaEmpleados[position];
+
+        layout.tvNombreEMMOD.text = elementoActual.nombre.toString();
+        layout.tvApellidosEMMOD.text = elementoActual.apellidos.toString();
+        layout.tvTelefonoEMMOD.text = elementoActual.telefono
+        layout.tvDireccionEMMOD.text = elementoActual.direccion;
+
+        if (elementoActual.avatar.isEmpty()){
+
+            layout.avatar.setImageResource(R.drawable.pixlr_bg_result)
+
+        }
+        else{
+
+
+        }
+
+        layout.btnEditarEMMOD.setOnClickListener(){
+
+
+        }
+
+
+        return layout;
+    }
+
+}
