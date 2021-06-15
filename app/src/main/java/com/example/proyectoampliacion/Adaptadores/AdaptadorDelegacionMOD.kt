398aaa0ddd406fe2265003e77c8c6a38a78f4038
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-06-15 12:04:32 +0200

    Anñadidas vista de modificar  delegacion

---
+++ app/src/main/java/com/example/proyectoampliacion/Adaptadores/AdaptadorDelegacionMOD.kt
@@ -0,0 +1,36 @@
+package com.example.proyectoampliacion.Adaptadores
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.proyectoampliacion.Classes_Auxiliares.Delegacion
+import com.example.proyectoampliacion.R
+import kotlinx.android.synthetic.main.vista_delegacion.view.*
+import kotlinx.android.synthetic.main.vista_mod_delegacion.view.*
+
+class AdaptadorDelegacionMOD(private val mContext: Context, private val listaDelegaciones: List<Delegacion>):
+    ArrayAdapter<Delegacion>(mContext,0,listaDelegaciones) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        //return super.getView(position, convertView, parent)
+
+        val layout = LayoutInflater.from(mContext).inflate(R.layout.vista_mod_delegacion,parent,false);
+
+        val elementoActual = listaDelegaciones[position];
+
+        layout.tvNombreDMOD.text = elementoActual.nombre;
+        layout.tvDireccionDMOD.text = elementoActual.direccion;
+        layout.tvEmailDMOD.text = elementoActual.email;
+        layout.tvTelefonoDMOD.text = elementoActual.telefono;
+
+
+        layout.btnEditarDMOD.setOnClickListener(){
+
+        }
+
+        return layout;
+    }
+}
