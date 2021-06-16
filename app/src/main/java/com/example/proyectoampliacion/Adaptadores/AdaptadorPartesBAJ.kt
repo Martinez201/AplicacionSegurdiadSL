b76df577140cfe80b05883d502b05f546c67cd95
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2021-06-17 01:29:12 +0200

    Terminado mecanismo para borrar partes

--- app/src/main/java/com/example/proyectoampliacion/Adaptadores/AdaptadorPartesBAJ.kt
+++ app/src/main/java/com/example/proyectoampliacion/Adaptadores/AdaptadorPartesBAJ.kt
@@ -8,13 +8,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.proyectoampliacion.Classes_Auxiliares.Parte
 import com.example.proyectoampliacion.R
 import kotlinx.android.synthetic.main.vista_baj_partes.view.*
 import kotlinx.android.synthetic.main.vista_mod_partes.view.*
+import okhttp3.Call
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import org.json.JSONObject
+import java.io.IOException
 
 class AdaptadorPartesBAJ (private val mContext: Context, private val listaPartes: List<Parte>): ArrayAdapter<Parte>(mContext,0,listaPartes) {
+
+
+    val URL_BASE:String = "http://192.168.1.141/symfony/web/app.php/"
+
+    var elemento:Int = 0;
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
@@ -47,10 +59,7 @@ class AdaptadorPartesBAJ (private val mContext: Context, private val listaPartes
 
         layout.btnBorrarPARBAJ.setOnClickListener(){
 
-            var bundle = Bundle();
-
-            bundle.putInt("elemento",elementoActual.id)
-
+            elemento = elementoActual.id
             dialogoBorrar()
 
         }
@@ -68,6 +77,7 @@ class AdaptadorPartesBAJ (private val mContext: Context, private val listaPartes
 
         builder.setPositiveButton("SI") { dialog, which ->
 
+            borrarParte()
         }
 
         builder.setNegativeButton("NO") { dialog, which ->
@@ -76,6 +86,48 @@ class AdaptadorPartesBAJ (private val mContext: Context, private val listaPartes
 
         builder.show()
 
+    }
+
+    fun borrarParte(){
+
+        var JSON: MediaType =  MediaType.get("application/json; charset=utf-8")
+
+        val jsonObject= JSONObject();
+
+        jsonObject.put("id",elemento);
+
+
+        val client = OkHttpClient()
+
+        val body: RequestBody = RequestBody.create(JSON,jsonObject.toString())
+
+        val request: okhttp3.Request = okhttp3.Request.Builder() //Create a request
+            .url(URL_BASE+"movil/parte/borrar")
+            .post(body) //Indicated as get request
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .build()
+
+        var llamada: Call = client.newCall(request)
+
+        try {
+
+            var response = llamada.execute()
+
+
+            if (response.body()?.string().toString().contains("Succes")){
+
+                Toast.makeText(this.context,"Parte borrado con éxito", Toast.LENGTH_SHORT).show()
+            }
+            else{
+
+                Toast.makeText(this.context,"Error: no se ha podido borrar el Parte", Toast.LENGTH_SHORT).show()
+            }
+
+        }catch (e: IOException){
+
+            Toast.makeText(this.context,e.message.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
